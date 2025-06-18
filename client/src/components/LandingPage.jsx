31b294d917855ecde1d4c93d999bce254d4c5dc3
Anant-4-code <anantrai0809@gmail.com>
2025-06-18 20:35:41 +0530

    Fix: Import FiArrowRight and remove unused React import in LandingPage.jsx

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { 
   FiMessageSquare, 
@@ -6,7 +5,8 @@ import {
   FiSun, 
   FiZap,
   FiLock,
-  FiSmartphone
+  FiSmartphone,
+  FiArrowRight
 } from 'react-icons/fi';
 
 const FeatureCard = ({ icon, title, description }) => (
